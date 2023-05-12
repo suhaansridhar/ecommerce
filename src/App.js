bf3c34da6b78bce14f58a2d389797ffe2df14686
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-05-12 14:20:19 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className="main--container">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AmazonLogo from '../Amazon_logo.png';
+
+
+const NavBar = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default NavBar
